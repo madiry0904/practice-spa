e4967fb6755a778046d2bbaef8b34bedbc374573
Madi Ryan <madi.ryan92@gmail.com>
2019-12-30 21:29:35 -0600

    add form submission for netlify

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
 //querySelector accepts ANY CSS Selector as a string
 
 const form = document.querySelector("form");
+/**
+ * 1. Listen for an Event
+ * 2. . Use a callback function when this even occurs
+ */
 
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log("I am submitted");
+});
 console.log(form);
