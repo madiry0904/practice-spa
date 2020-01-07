a9156018018edbce9deb9569836f89f877db085c
Madi Ryan <madi.ryan92@gmail.com>
2020-01-06 21:28:36 -0600

    render the correct heading in response to mouse clicks using a render function

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ const state = {
   }
 };
 
-document.querySelector("#root").innerHTML = `${Header(state.Home)}
-${Main()}
- ${Footer()}`;
+function render(st = state.Home) {
+  document.querySelector("#root").innerHTML = `${Header(st)}
+ ${Main(st)}
+ ${Footer(st)}`;
+}
+render();
 
+//renders your page before you start to query the page
 //TODO LISTEN FOR CLICKS ON OUR MENU AND LOG WHAT WAS CLICKED
 
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(event.target.textContent);
+    //console.log(event.target.textContent);
 
     //TODO: USE THIS INFORMATION AND LOG OUT THE CORRECT PIECE OF STATE
-    console.log(state[event.target.textContent]); //MUST USE BRACKET NOTATION HERE
+    render(state[event.target.textContent]); //MUST USE BRACKET NOTATION HERE
   });
 });
