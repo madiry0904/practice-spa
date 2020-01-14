751b8f54bd679630102510c270c95ffb189c1bcd
Madi Ryan <madi.ryan92@gmail.com>
2020-01-13 21:29:30 -0600

    add dotenv

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+export default {
+  apiKey: `${process.env.FIRESTORE_API}`,
+  authDomain: "savvy-practice-2020.firebaseapp.com",
+  databaseURL: "https://savvy-practice-2020.firebaseio.com",
+  projectId: "savvy-practice-2020",
+  storageBucket: "savvy-practice-2020.appspot.com",
+  messagingSenderId: "672470489636",
+  appId: "1:672470489636:web:61b1ee24571b2bec3956b1"
+};
